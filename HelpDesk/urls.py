81f8d555cd5c74c7421ebe0b2481ecd0c41470c7
Urmit Gajjar <urmit.gajjar@trilokninfotech.com>
2026-03-12 14:35:33 +0530

    updates

--- HelpDesk/urls.py
+++ HelpDesk/urls.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.http import HttpResponse
 
 
 def favicon_view(_request):
-    # Return "No Content" so browsers stop generating noisy 404s.
     return HttpResponse(status=204)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -16,11 +15,13 @@ urlpatterns = [
     path("favicon.ico", favicon_view, name="favicon"),
 ]
 
-if settings.DEBUG or getattr(settings, "IS_RUNSERVER", False):
-    urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
-    urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 admin.site.site_header = "Helpdesk Administration"
-admin.site.site_title  = "Helpdesk Admin Portal"
+admin.site.site_title = "Helpdesk Admin Portal"
 admin.site.index_title = "Welcome to Helpdesk Admin Panel"
 
